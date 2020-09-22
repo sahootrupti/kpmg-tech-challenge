c01b351b54a6e6a6b9079a330ef4f80ca76b749c
sahootrupti <truptiranjan999@gmail.com>
2020-09-22 16:57:17 +0530

    Add files via upload

---
+++ Tech_Challenge-2/script_metadata_method-1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+aws ec2 describe-instance-attribute --instance-id <EC2_instance_id> --attribute <mention_below_any_attribute> --region <your_region>
---
+++ Tech_Challenge-2/script_metadata_method-2.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -X GET http://169.254.169.254/latest/meta-data/
